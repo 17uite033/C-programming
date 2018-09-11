4ed36cdd1e7a53a39768688160f9cc657b22ff95
17uite033 <38652951+17uite033@users.noreply.github.com>
2018-09-11 10:22:07 +0000

    Create multi.c

---
+++ multi.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+void main()
+{
+                int n,i,s;
+                scanf("%d",&n);
+                for(i=1;i<=5;i++)
+                {
+                               s=n*i;
+                               printf("\n%d",s);
+                 }
+                
+}
