cfe858ab6a1ca9da5be74a34aee406bfe702ed28
17uite033 <38652951+17uite033@users.noreply.github.com>
2018-09-18 11:05:33 +0000

    Create max.c

---
+++ max.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+        int a[30];
+        int n,t,i,max;
+        scanf("%d",&n);
+        scanf("%d",&t);
+        for(i=1;i<n;i++)
+        {
+                scanf("%d",&a[i]);
+        }
+        max=a[0];
+        for(i=0;i<n;i++)
+        {
+                if(a[i]>max)
+                {
+                        max=a[i];
+                }
+        }
+        printf("%d",max);
+        return 0;
+}
